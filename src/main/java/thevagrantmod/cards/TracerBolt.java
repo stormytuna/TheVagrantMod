4a406507194d2c5c896d0adbd421d3bc889a7ba1
stormytuna <stormytuna@outlook.com>
2026-01-22 15:12:37 +0000

    add tracer bolt

---
+++ src/main/java/thevagrantmod/cards/TracerBolt.java
@@ -0,0 +1,45 @@
+package thevagrantmod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+
+import thevagrantmod.TheVagrantMod;
+import thevagrantmod.character.TheVagrant;
+import thevagrantmod.util.CardStats;
+
+public class TracerBolt extends BaseCard {
+    public static final String ID = TheVagrantMod.makeID("TracerBolt");
+
+    private static final CardStats INFO = new CardStats(
+        TheVagrant.Meta.CARD_COLOR,
+        CardType.ATTACK,
+        CardRarity.COMMON,
+        CardTarget.ENEMY,
+        1
+    );
+
+    public TracerBolt() {
+        super(ID, INFO);
+        setDamage(8, 3);
+        setMagic(1, 1);
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new TracerBolt();
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageType.NORMAL), AttackEffect.BLUNT_LIGHT));
+        addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, magicNumber, baseEthereal)));
+    }
+}
+
