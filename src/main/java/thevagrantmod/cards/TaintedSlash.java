0be240c2cd0ae1c6443ba364b2deaad0130a797e
stormytuna <stormytuna@outlook.com>
2026-01-28 04:03:28 +0000

    add tainted slash, fix rangefinding cost

--- src/main/java/thevagrantmod/cards/TaintedSlash.java
+++ src/main/java/thevagrantmod/cards/TaintedSlash.java
@@ -1,8 +1,8 @@
 package thevagrantmod.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.UpgradeRandomCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -10,42 +10,36 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import thevagrantmod.TheVagrantMod;
 import thevagrantmod.character.TheVagrant;
+import thevagrantmod.powers.BlightPower;
 import thevagrantmod.util.CardStats;
 
-public class Rangefinding extends BaseCard {
-    public static final String ID = TheVagrantMod.makeID("Rangefinding");
+public class TaintedSlash extends BaseCard {
+    public static final String ID = TheVagrantMod.makeID("TaintedSlash");
 
     private static final CardStats INFO = new CardStats(
         TheVagrant.Meta.CARD_COLOR,
         CardType.ATTACK,
         CardRarity.COMMON,
         CardTarget.ENEMY,
-        2
+        0
     );
 
-    private static final int NUM_ATTACKS = 2;
-    private static final int NUM_UPGRADES = 2;
-
-    public Rangefinding() {
+    public TaintedSlash() {
         super(ID, INFO);
-        setDamage(6, 2);
+        setDamage(3, 1);
+        setMagic(2, 1);
     }
 
     @Override
     public AbstractCard makeCopy() {
-        return new Rangefinding();
+        return new TaintedSlash();
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        for (int i = 0; i < NUM_ATTACKS; i++) {
-            DamageInfo info = new DamageInfo(p, damage);
-            addToBot(new DamageAction(m, info, AttackEffect.BLUNT_LIGHT));
-        }
-
-        for (int i = 0; i < NUM_UPGRADES; i++) {
-            addToBot(new UpgradeRandomCardAction());
-        }
+        DamageInfo info = new DamageInfo(p, damage);
+        addToBot(new DamageAction(m, info, AttackEffect.SLASH_HEAVY));
+        addToBot(new ApplyPowerAction(m, p, new BlightPower(m, magicNumber)));
     }
 }
 
