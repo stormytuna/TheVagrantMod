8d40b7c8e8c107c5d82d9dd521af350b7764c585
stormytuna <stormytuna@outlook.com>
2026-02-04 23:04:54 +0000

    add formatter, format source

--- src/main/java/thevagrantmod/cardModifiers/JammedModifier.java
+++ src/main/java/thevagrantmod/cardModifiers/JammedModifier.java
@@ -1,7 +1,7 @@
 package thevagrantmod.cardModifiers;
 
-import java.time.Duration;
-
+import basemod.abstracts.AbstractCardModifier;
+import basemod.helpers.CardModifierManager;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.ByRef;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInstrumentPatch;
@@ -9,7 +9,6 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
-import com.megacrit.cardcrawl.actions.unique.UndoAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.AbstractCard.CardType;
@@ -19,9 +18,6 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ShaderHelper;
 import com.megacrit.cardcrawl.localization.UIStrings;
-
-import basemod.abstracts.AbstractCardModifier;
-import basemod.helpers.CardModifierManager;
 import javassist.CannotCompileException;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
@@ -31,145 +27,161 @@ import thevagrantmod.effects.JamCardEffect;
 import thevagrantmod.shaders.BetterGrayscaleShader;
 
 public class JammedModifier extends AbstractCardModifier {
-    public static final String ID = TheVagrantMod.makeID("JammedModifier");
+  public static final String ID = TheVagrantMod.makeID("JammedModifier");
 
-    private static final UIStrings STRINGS = CardCrawlGame.languagePack.getUIString(ID);
+  private static final UIStrings STRINGS = CardCrawlGame.languagePack.getUIString(ID);
 
-    public static boolean canJam(AbstractCard card) {
-        if (CardModifierManager.hasModifier(card, ID) || card.type == CardType.STATUS || card.type == CardType.CURSE || card.cost == -2) {
-            return false;
-        }
-
-        return true;
+  public static boolean canJam(AbstractCard card) {
+    if (CardModifierManager.hasModifier(card, ID)
+        || card.type == CardType.STATUS
+        || card.type == CardType.CURSE
+        || card.cost == -2) {
+      return false;
     }
 
-    @Override
-    public String identifier(AbstractCard card) {
-        return ID;
+    return true;
+  }
+
+  @Override
+  public String identifier(AbstractCard card) {
+    return ID;
+  }
+
+  @Override
+  public boolean removeOnCardPlayed(AbstractCard card) {
+    return true;
+  }
+
+  @Override
+  public AbstractCardModifier makeCopy() {
+    return new JammedModifier();
+  }
+
+  @Override
+  public String modifyDescription(String rawDescription, AbstractCard card) {
+    return STRINGS.TEXT[0] + rawDescription;
+  }
+
+  @SpirePatch2(clz = AbstractPlayer.class, method = "useCard")
+  public static class JammedCardDoNoEffect {
+    @SpireInstrumentPatch
+    public static ExprEditor patch() {
+      return new ExprEditor() {
+        @Override
+        public void edit(MethodCall m) throws CannotCompileException {
+          if (m.getClassName().equals(AbstractCard.class.getName())
+              && m.getMethodName().equals("use")) {
+            m.replace(
+                "if ("
+                    + JammedCardDoNoEffect.class.getName()
+                    + ".isJammed(c)) {"
+                    + "  "
+                    + JammedCardDoNoEffect.class.getName()
+                    + ".unjam(c);"
+                    + "} else {"
+                    + "  $proceed($$);"
+                    + "}");
+          }
+        }
+      };
     }
 
-    @Override
-    public boolean removeOnCardPlayed(AbstractCard card) {
-        return true;
+    public static boolean isJammed(AbstractCard c) {
+      return CardModifierManager.hasModifier(c, ID);
     }
 
-    @Override
-    public AbstractCardModifier makeCopy() {
-        return new JammedModifier();
+    public static void unjam(AbstractCard c) {
+      AbstractDungeon.actionManager.addToBottom(new UnjamSpecificCardAction(c));
+    }
+  }
+
+  @SpirePatch2(
+      clz = UseCardAction.class,
+      method = SpirePatch.CONSTRUCTOR,
+      paramtypez = {AbstractCard.class, AbstractCreature.class})
+  public static class JammedCardNoExhaust {
+    @SpirePostfixPatch
+    public static void patch(AbstractCard ___targetCard, @ByRef boolean[] ___exhaustCard) {
+      if (CardModifierManager.hasModifier(___targetCard, ID)) {
+        ___exhaustCard[0] = false;
+      }
+    }
+  }
+
+  // Making it a skill in ctor as UseCardAction is in the action queue *after* our
+  // UnjamSpecificCardAction,
+  //   so would require weird hacks to remember it was jammed
+  @SpirePatch2(
+      clz = UseCardAction.class,
+      method = SpirePatch.CONSTRUCTOR,
+      paramtypez = {AbstractCard.class, AbstractCreature.class})
+  public static class JammedPowerToSkillSoItDoesntGetEaten {
+    @SpirePostfixPatch
+    public static void patch(AbstractCard ___targetCard) {
+      if (CardModifierManager.hasModifier(___targetCard, ID)
+          && ___targetCard.type == CardType.POWER) {
+        ___targetCard.type = CardType.SKILL;
+        JammedPowerBePowerAgain.undoUnpowering = true;
+      }
+    }
+  }
+
+  @SpirePatch2(clz = UseCardAction.class, method = "update")
+  public static class JammedPowerBePowerAgain {
+    private static boolean undoUnpowering = false;
+
+    @SpirePostfixPatch
+    public static void patch2(AbstractCard ___targetCard, boolean ___isDone) {
+      if (undoUnpowering) {
+        ___targetCard.type = CardType.POWER;
+        undoUnpowering = false;
+      }
+    }
+  }
+
+  @SpirePatch2(clz = AbstractCard.class, method = "renderPortrait")
+  @SpirePatch2(clz = AbstractCard.class, method = "renderJokePortrait")
+  public static class JammedGrayscalePortrait {
+    private static boolean undoGrayShader = false;
+
+    @SpirePrefixPatch
+    public static void applyShader(AbstractCard __instance, SpriteBatch sb) {
+      if (JamCardEffect.Fields.grayscaleStrength.get(__instance) > 0) {
+        BetterGrayscaleShader.apply(sb, JamCardEffect.Fields.grayscaleStrength.get(__instance));
+        undoGrayShader = true;
+      }
     }
 
-    @Override
-    public String modifyDescription(String rawDescription, AbstractCard card) {
-        return STRINGS.TEXT[0] + rawDescription;
+    @SpirePostfixPatch
+    public static void unapplyShader(AbstractCard __instance, SpriteBatch sb) {
+      if (undoGrayShader) {
+        ShaderHelper.setShader(sb, ShaderHelper.Shader.DEFAULT);
+        undoGrayShader = false;
+      }
+    }
+  }
+
+  @SpirePatch2(clz = AbstractCard.class, method = "renderCardBg")
+  @SpirePatch2(clz = AbstractCard.class, method = "renderPortraitFrame")
+  @SpirePatch2(clz = AbstractCard.class, method = "renderBannerImage")
+  public static class JammedGrayscaleCardBits {
+    private static boolean undoGrayShader = false;
+
+    @SpirePrefixPatch
+    public static void applyShader(AbstractCard __instance, SpriteBatch sb) {
+      if (JamCardEffect.Fields.grayscaleStrength.get(__instance) > 0) {
+        BetterGrayscaleShader.apply(
+            sb, JamCardEffect.Fields.grayscaleStrength.get(__instance) * 0.5f);
+        undoGrayShader = true;
+      }
     }
 
-    @SpirePatch2(clz = AbstractPlayer.class, method = "useCard")
-    public static class JammedCardDoNoEffect {
-        @SpireInstrumentPatch
-        public static ExprEditor patch() {
-            return new ExprEditor() {
-                @Override
-                public void edit(MethodCall m) throws CannotCompileException {
-                    if (m.getClassName().equals(AbstractCard.class.getName()) && m.getMethodName().equals("use")) {
-                        m.replace(
-                                "if (" + JammedCardDoNoEffect.class.getName() + ".isJammed(c)) {" +
-                                "  " + JammedCardDoNoEffect.class.getName() + ".unjam(c);" +
-                                "} else {" +
-                                "  $proceed($$);" +
-                                "}"
-                            ); 
-                    }
-                }
-            };
-        }
-
-        public static boolean isJammed(AbstractCard c) {
-            return CardModifierManager.hasModifier(c, ID);
-        }
-
-        public static void unjam(AbstractCard c) {
-            AbstractDungeon.actionManager.addToBottom(new UnjamSpecificCardAction(c));
-        }
+    @SpirePostfixPatch
+    public static void unapplyShader(AbstractCard __instance, SpriteBatch sb) {
+      if (undoGrayShader) {
+        ShaderHelper.setShader(sb, ShaderHelper.Shader.DEFAULT);
+        undoGrayShader = false;
+      }
     }
-
-    @SpirePatch2(clz = UseCardAction.class, method = SpirePatch.CONSTRUCTOR, paramtypez = {AbstractCard.class, AbstractCreature.class})
-    public static class JammedCardNoExhaust {
-        @SpirePostfixPatch
-        public static void patch(AbstractCard ___targetCard, @ByRef boolean[] ___exhaustCard) {
-            if (CardModifierManager.hasModifier(___targetCard, ID)) {
-                ___exhaustCard[0] = false;
-            }
-        }
-    }
-
-    // Making it a skill in ctor as UseCardAction is in the action queue *after* our UnjamSpecificCardAction,
-    //   so would require weird hacks to remember it was jammed
-    @SpirePatch2(clz = UseCardAction.class, method = SpirePatch.CONSTRUCTOR, paramtypez = {AbstractCard.class, AbstractCreature.class})
-    public static class JammedPowerToSkillSoItDoesntGetEaten {
-        @SpirePostfixPatch
-        public static void patch(AbstractCard  ___targetCard) {
-            if (CardModifierManager.hasModifier(___targetCard, ID) && ___targetCard.type == CardType.POWER) {
-                ___targetCard.type = CardType.SKILL;
-                JammedPowerBePowerAgain.undoUnpowering = true;
-            }
-        }
-    }
-
-    @SpirePatch2(clz = UseCardAction.class, method = "update")
-    public static class JammedPowerBePowerAgain {
-        private static boolean undoUnpowering = false;
-
-        @SpirePostfixPatch
-        public static void patch2(AbstractCard ___targetCard, boolean ___isDone) {
-            if (undoUnpowering) {
-                ___targetCard.type = CardType.POWER;
-                undoUnpowering = false;
-            }
-        }
-    }
-
-    @SpirePatch2(clz = AbstractCard.class, method = "renderPortrait")
-    @SpirePatch2(clz = AbstractCard.class, method = "renderJokePortrait")
-    public static class JammedGrayscalePortrait {
-        private static boolean undoGrayShader = false;
-
-        @SpirePrefixPatch
-        public static void applyShader(AbstractCard __instance, SpriteBatch sb) {
-            if (JamCardEffect.Fields.grayscaleStrength.get(__instance) > 0) {
-                BetterGrayscaleShader.apply(sb, JamCardEffect.Fields.grayscaleStrength.get(__instance));
-                undoGrayShader = true;
-            }
-        }
-
-        @SpirePostfixPatch
-        public static void unapplyShader(AbstractCard __instance, SpriteBatch sb) {
-            if (undoGrayShader) {
-                ShaderHelper.setShader(sb, ShaderHelper.Shader.DEFAULT);
-                undoGrayShader = false;
-            }
-        }
-    }
-
-    @SpirePatch2(clz = AbstractCard.class, method = "renderCardBg")
-    @SpirePatch2(clz = AbstractCard.class, method = "renderPortraitFrame")
-    @SpirePatch2(clz = AbstractCard.class, method = "renderBannerImage")
-    public static class JammedGrayscaleCardBits {
-        private static boolean undoGrayShader = false;
-
-        @SpirePrefixPatch
-        public static void applyShader(AbstractCard __instance, SpriteBatch sb) {
-            if (JamCardEffect.Fields.grayscaleStrength.get(__instance) > 0) {
-                BetterGrayscaleShader.apply(sb, JamCardEffect.Fields.grayscaleStrength.get(__instance) * 0.5f);
-                undoGrayShader = true;
-            }
-        }
-
-        @SpirePostfixPatch
-        public static void unapplyShader(AbstractCard __instance, SpriteBatch sb) {
-            if (undoGrayShader) {
-                ShaderHelper.setShader(sb, ShaderHelper.Shader.DEFAULT);
-                undoGrayShader = false;
-            }
-        }
-    }
+  }
 }
