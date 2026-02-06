ba03f0270acfe627a883bfc676fd3cde8ca50b63
stormytuna <stormytuna@outlook.com>
2026-02-06 22:30:47 +0000

    change resolute form to gain energy

--- src/main/java/thevagrantmod/powers/ResoluteFormPower.java
+++ src/main/java/thevagrantmod/powers/ResoluteFormPower.java
@@ -1,12 +1,7 @@
 package thevagrantmod.powers;
 
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
-import com.megacrit.cardcrawl.actions.utility.UseCardAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.rooms.AbstractRoom.RoomPhase;
 
 import thevagrantmod.TheVagrantMod;
 
@@ -16,50 +11,18 @@ public class ResoluteFormPower extends BasePower {
     private static final PowerType TYPE = PowerType.BUFF;
     private static final boolean TURN_BASED = false;
 
-    private int cardsToMakeFreeEachTurn;
-
     public ResoluteFormPower(AbstractCreature owner, int amount) {
-        super(ID, TYPE, TURN_BASED, owner, owner, amount, false);
-        cardsToMakeFreeEachTurn = amount;
-        updateDescription();
+        super(ID, TYPE, TURN_BASED, owner, amount);
     }
 
     @Override
     public void updateDescription() {
-        description = DESCRIPTIONS[0] + cardsToMakeFreeEachTurn + DESCRIPTIONS[1];
+        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
     }
 
     @Override
-    public void stackPower(int stackAmount) {
-        super.stackPower(stackAmount);
-        cardsToMakeFreeEachTurn += stackAmount;
-    }
-
-    @Override
-    public void atStartOfTurn() {
-        amount = cardsToMakeFreeEachTurn;
-    }
-
-    @Override
-    public void onUseCard(AbstractCard card, UseCardAction action) {
-        if (!card.purgeOnUse && amount > 0) {
-            amount--;
-            flash();
-        }
-    }
-
-    @SpirePatch2(clz = AbstractCard.class, method = "freeToPlay")
-    public static class MakeCardsFree {
-        @SpirePostfixPatch
-        public static boolean patch(AbstractCard __instance, boolean __result) {
-            if (AbstractDungeon.player != null && AbstractDungeon.currMapNode != null && AbstractDungeon.getCurrRoom().phase == RoomPhase.COMBAT && AbstractDungeon.player.hasPower(ID)) {
-                ResoluteFormPower power = (ResoluteFormPower)AbstractDungeon.player.getPower(ID);
-                if (power.amount > 0) {
-                    return true;
-                }
-            }
-
-            return __result;
-        }
+    public void onEnergyRecharge() {
+        flash();
+        AbstractDungeon.player.gainEnergy(amount);
     }
 }
