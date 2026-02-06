7c1cab2ad281b25c5eebb4a87adf866e32b9c118
stormytuna <stormytuna@outlook.com>
2026-02-06 18:33:39 +0000

    add refining shot

---
+++ src/main/java/thevagrantmod/cards/RefiningShot.java
@@ -0,0 +1,42 @@
+package thevagrantmod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.unique.ArmamentsAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import thevagrantmod.TheVagrantMod;
+import thevagrantmod.character.TheVagrant;
+import thevagrantmod.util.CardStats;
+
+public class RefiningShot extends BaseCard {
+    public static final String ID = TheVagrantMod.makeID("RefiningShot");
+
+    private static final CardStats INFO = new CardStats(
+        TheVagrant.Meta.CARD_COLOR,
+        CardType.ATTACK,
+        CardRarity.UNCOMMON,
+        CardTarget.ENEMY,
+        1
+    );
+
+    public RefiningShot() {
+        super(ID, INFO);
+        setDamage(6, 2);
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new RefiningShot();
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage), AttackEffect.BLUNT_LIGHT));
+        addToBot(new ArmamentsAction(true));
+    }
+}
+
