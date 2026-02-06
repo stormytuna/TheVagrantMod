857b6f15509464ba2e14cd7a82427df2324bcb31
stormytuna <stormytuna@outlook.com>
2026-02-06 22:23:04 +0000

    add together in spire compat, add traps as a name for trap keyword

---
+++ src/main/java/thevagrantmod/patches/TogetherInSpireCompat.java
@@ -0,0 +1,35 @@
+package thevagrantmod.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+
+import spireTogether.util.SpireHelp;
+import thevagrantmod.powers.BlightPower;
+import thevagrantmod.powers.FlamingBoltPower;
+
+public class TogetherInSpireCompat {
+    @SpirePatch2(clz = BlightPower.class, method = "doBlightHpLoss", requiredModId = "spireTogether")
+    public static class BlightHpLossHostOnly {
+        @SpirePrefixPatch
+        public static SpireReturn<Void> patch() {
+            if (SpireHelp.Multiplayer.IsHost()) {
+                return SpireReturn.Continue();
+            }
+
+            return SpireReturn.Return();
+        }
+    }
+
+    @SpirePatch2(clz = FlamingBoltPower.class, method = "atEndOfTurn", requiredModId = "spireTogether")
+    public static class FlamingBoltDamageHostOnly {
+        @SpirePrefixPatch
+        public static SpireReturn<Void> patch() {
+            if (SpireHelp.Multiplayer.IsHost()) {
+                return SpireReturn.Continue();
+            }
+
+            return SpireReturn.Return();
+        }
+    }
+}
