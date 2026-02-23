6c5676bf55871551ae9432b2bdc3f75b16abe8f8
Typhlosionater <Typhlosionater@gmail.com>
2026-02-23 19:01:59 +0000

    Update explosive bolt vfx

--- src/main/java/thevagrantmod/cards/ExplosiveBolt.java
+++ src/main/java/thevagrantmod/cards/ExplosiveBolt.java
@@ -1,12 +1,19 @@
 package thevagrantmod.cards;
 
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.combat.ClawEffect;
+import com.megacrit.cardcrawl.vfx.combat.ExplosionSmallEffect;
 import thevagrantmod.TheVagrantMod;
 import thevagrantmod.actions.JamSpecificCardAction;
 import thevagrantmod.character.TheVagrant;
@@ -35,7 +42,12 @@ public class ExplosiveBolt extends BaseCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new DamageAllEnemiesAction(p, damage, DamageType.NORMAL, AttackEffect.BLUNT_HEAVY));
+        for (AbstractMonster monster : (AbstractDungeon.getMonsters()).monsters) {
+            if (!monster.isDeadOrEscaped()) {
+                addToBot((AbstractGameAction)new VFXAction((AbstractGameEffect) new ExplosionSmallEffect(monster.hb.cX, monster.hb.cY), 0.1F));
+            }
+        }
+        addToBot(new DamageAllEnemiesAction(p, damage, DamageType.NORMAL, AttackEffect.NONE));
         addToBot(new JamSpecificCardAction(this));
     }
 }
